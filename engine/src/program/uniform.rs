af1f5c0fd3d707fb72671e59560991ae36d2f191
Jeremy5909 <jeremyherczeg@gmail.com>
2025-06-10 17:15:19 -0700

    few more UniformValue impls

--- engine/src/program/uniform.rs
+++ engine/src/program/uniform.rs
@@ -16,6 +16,22 @@ impl Program {
 pub trait UniformValue {
     fn set_uniform(&self, location: i32);
 }
+
+// Numbers
+impl UniformValue for bool {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform1i(location, *self as i32);
+        }
+    }
+}
+impl UniformValue for f32 {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform1f(location, *self);
+        }
+    }
+}
 impl UniformValue for i32 {
     fn set_uniform(&self, location: i32) {
         unsafe {
@@ -23,6 +39,8 @@ impl UniformValue for i32 {
         }
     }
 }
+
+// Vectors
 impl UniformValue for Vector3<f32> {
     fn set_uniform(&self, location: i32) {
         unsafe {
@@ -37,6 +55,52 @@ impl UniformValue for Vector4<f32> {
         }
     }
 }
+
+// Tuples
+impl UniformValue for (i32, i32) {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform2i(location, self.0, self.1);
+        }
+    }
+}
+impl UniformValue for (f32, f32) {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform2f(location, self.0, self.1);
+        }
+    }
+}
+impl UniformValue for (i32, i32, i32) {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform3i(location, self.0, self.1, self.2);
+        }
+    }
+}
+impl UniformValue for (f32, f32, f32) {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform3f(location, self.0, self.1, self.2);
+        }
+    }
+}
+impl UniformValue for (i32, i32, i32, i32) {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform4i(location, self.0, self.1, self.2, self.3);
+        }
+    }
+}
+impl UniformValue for (f32, f32, f32, f32) {
+    fn set_uniform(&self, location: i32) {
+        unsafe {
+            gl::Uniform4f(location, self.0, self.1, self.2, self.3);
+        }
+    }
+}
+
+// Matrices
 impl UniformValue for Matrix3<f32> {
     fn set_uniform(&self, location: i32) {
         unsafe {
